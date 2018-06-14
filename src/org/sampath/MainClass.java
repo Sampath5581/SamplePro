15cc95ce5d76d8453d9138ce51ef380c8d285f72
Sampath5581 <sampathgoud85@gmail.com>
2018-06-14 14:55:49 +0530

    test1

---
+++ src/org/sampath/MainClass.java
@@ -0,0 +1,10 @@
+package org.sampath;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("Sample Java Pro");
+	}
+
+}
