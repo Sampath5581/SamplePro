aa0ca94c7cfad1eedda89e4489adb113d7e2cf3a
Sampath5581 <40229064+Sampath5581@users.noreply.github.com>
2018-07-10 16:19:29 +0530

    Update MainClass.java

--- src/org/sampath/MainClass.java
+++ src/org/sampath/MainClass.java
@@ -5,6 +5,9 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub test
      System.out.println("Sample Java Pro");
+		int i=20;
+		System.out.println(i);
+		
 	}
 
 }
